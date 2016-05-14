7627307529ccb4a701df8deb800f02e2efc2eda6
GT <GT@183.97.180.149>
2016-05-14 20:05:46 +0900

    merge MyButton

--- src/GameScene/GameScene.java
+++ src/GameScene/GameScene.java
@@ -2,11 +2,13 @@ package GameScene;
 
 import javax.swing.JSplitPane;
 
+
 public class GameScene extends JSplitPane {
 	public GameScene() {
 		setOrientation(JSplitPane.HORIZONTAL_SPLIT);
 		setDividerLocation(600);
 		setEnabled(false);// Á¶Á¤
+		setDividerSize(5);
 
 		GamePanel gamePanel = new GamePanel();
 		setLeftComponent(gamePanel);
--- src/kakaoTyping/MainApp.java
+++ src/kakaoTyping/MainApp.java
@@ -1,11 +1,7 @@
 package kakaoTyping;
 
 public class MainApp {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		new MyFrame();
-		
+	public static void main(String[] args) {		
+		new MyFrame();		
 	}
-
 }
