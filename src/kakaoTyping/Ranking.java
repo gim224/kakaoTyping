5376007edd8d71cba275a8c5cb8f1a4b3c285e34
jjy <chjy1116@gmail.com>
2016-05-15 23:02:15 +0900

    Ranking
    Ranking

---
+++ src/kakaoTyping/Ranking.java
@@ -0,0 +1,20 @@
+package kakaoTyping;
+
+import java.awt.BorderLayout;
+
+import javax.swing.*;
+
+public class Ranking extends JPanel {
+	String header[] = { "순위", "이름", "점수", "목표 정확도", "오타율", "날짜" };
+	String contents[][] = {};
+
+	public Ranking() {
+		setLayout(new BorderLayout());
+		JTable table = new JTable(contents, header);
+		JScrollPane scrollpane = new JScrollPane(table);
+		add(scrollpane, BorderLayout.CENTER);
+		/*JButton bttn = new JButton("←Back");
+		bttn.setSize(130, 30);
+		add(bttn, BorderLayout.SOUTH);*/
+	}
+}
