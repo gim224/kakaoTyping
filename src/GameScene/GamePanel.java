99698a9b6bf870d2bce1a35cdc377a04022e1ca8
gim224 <c233c@naver.com>
2016-05-22 02:04:48 +0900

    FileInput 클래스 추가
    입출력의 용이함

--- src/GameScene/GamePanel.java
+++ src/GameScene/GamePanel.java
@@ -12,6 +12,8 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+import kakaoTyping.FileInput;
+
 public class GamePanel extends JPanel {
 	public GamePanel() {
 		setLayout(new BorderLayout());
@@ -31,14 +33,22 @@ class CreateWordPanel extends JPanel {
 
 	public CreateWordPanel() {
 		setLayout(null);
-		Vector<JLabel> word = new Vector<JLabel>();
-		JLabel wordLabel = new JLabel("zz");
-		wordLabel.setSize(100, 20);
-		wordLabel.setLocation(0, 0);
-		wordLabel.setOpaque(true);
-		wordLabel.setBackground(Color.YELLOW);
-		add(wordLabel);
-		word.add(wordLabel);
+		FileInput input = new FileInput("txt/word.txt");
+		
+		JLabel[] wordLabel = new JLabel[input.size()];
+		
+		for (int i = 0; i < input.size(); i++) {
+			wordLabel[i] = new JLabel(input.getOneWord(i));
+			wordLabel[i].setSize(100, 20);
+			wordLabel[i].setLocation(0, i*30);
+			wordLabel[i].setOpaque(true);
+			wordLabel[i].setBackground(Color.YELLOW);
+			add(wordLabel[i]);
+		}
+		
+		
+		//add(wordLabel);
+		//word.add(wordLabel);
 
 	}
 
